adf977ed4dbf1a38b7712554014a72f1944900ab
Donald Stufft <donald.stufft@gmail.com>
2011-07-30 15:03:20 -0400

    added the slumber package and added some metadata

---
+++ slumber/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "dev"
+
+__author__ = "Donald Stufft"
+__email__ = "donald.stufft@gmail.com"
+
+__description__ = "A library that makes consuming a ReST API easier and more convenient"
+__url__ = "https://github.com/dstufft/slumber/"
