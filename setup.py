d3bdc9fa99539e120347c6b4d2dc744a05187eb6
Donald Stufft <donald.stufft@gmail.com>
2011-08-02 18:38:36 -0400

    fix setup.py script

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name = "slumber",
     version = "0.2",
     description = "A library that makes consuming a ReST API easier and more convenient",
-    long_description=open("README.rst", "r").read() + open("CHANGELOG.rst", "r"),
+    long_description=open("README.rst", "r").read() + open("CHANGELOG.rst", "r").read(),
     url = "http://slumber.in/",
     author = "Donald Stufft",
     author_email = "donald.stufft@gmail.com",
