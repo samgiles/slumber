f1c564a636eed07d25cdd6975144bbe19d1c69b4
Donald Stufft <donald.stufft@gmail.com>
2012-07-07 12:51:00 -0400

    Be smarter about what we do with POST/PUT's returns

--- slumber/__init__.py
+++ slumber/__init__.py
@@ -123,12 +123,17 @@ class Resource(ResourceAttributesMixin, object):
 
         resp = self._request("POST", data=s.dumps(data), params=kwargs)
         if 200 <= resp.status_code <= 299:
-            if resp.status_code == 201:
+            if resp.headers.get("location"):
                 # @@@ Hacky, see description in __call__
                 resource_obj = self(url_override=resp.headers["location"])
                 return resource_obj.get(params=kwargs)
             else:
-                return resp.content
+                try:
+                    stype = s.get_serializer(content_type=resp.headers.get("content-type"))
+                except exceptions.SerializerNotAvailable:
+                    return resp.content
+
+                return stype.loads(resp.content)
         else:
             # @@@ Need to be Some sort of Error Here or Something
             return
@@ -138,12 +143,17 @@ class Resource(ResourceAttributesMixin, object):
 
         resp = self._request("PATCH", data=s.dumps(data), params=kwargs)
         if 200 <= resp.status_code <= 299:
-            if resp.status_code == 202:
+            if resp.headers.get("location"):
                 # @@@ Hacky, see description in __call__
                 resource_obj = self(url_override=resp.headers["location"])
                 return resource_obj.get(params=kwargs)
             else:
-                return resp.content
+                try:
+                    stype = s.get_serializer(content_type=resp.headers.get("content-type"))
+                except exceptions.SerializerNotAvailable:
+                    return resp.content
+
+                return stype.loads(resp.content)
         else:
             # @@@ Need to be Some sort of Error Here or Something
             return
