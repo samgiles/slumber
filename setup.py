3fa0221ab35520b147532d2afb9190bc82cdc170
SamGiles <samuel.giles@ft.com>
2014-11-11 15:20:04 +0000

    Preparing setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,17 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name = "slumber",
-    version = "0.6.1.dev",
+    version = "0.6.1",
     description = "A library that makes consuming a REST API easier and more convenient",
     long_description="\n\n".join([
         open(os.path.join(base_dir, "README.rst"), "r").read(),
         open(os.path.join(base_dir, "CHANGELOG.rst"), "r").read()
     ]),
-    url = "http://slumber.in/",
+    url = "http://github.com/samgiles/slumber",
     author = "Donald Stufft",
     author_email = "donald.stufft@gmail.com",
+    maintainer = "Samuel Giles",
+    maintainer_email = "sam.e.giles@gmail.com",
     packages = ["slumber"],
     zip_safe = False,
     install_requires = install_requires,
