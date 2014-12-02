ae9479d8f6c6b6b5806466f6d9e98b84b7ad810c
Jannon Frank <jannon.frank@tellagami.com>
2014-12-02 04:02:53 -0800

    fix for binary requests response content in py3
    This also includes a test that exposes the problem.  Namely that the
    response.content returned from requests on py3 is binary.  None of the
    tests previously accounted for this

--- tests/resource.py
+++ tests/resource.py
@@ -355,3 +355,32 @@ class ResourceTestCase(unittest.TestCase):
 
     def test_url(self):
         self.assertEqual(self.base_resource.url(), "http://example/api/v1/test")
+
+    def test_get_200_json_py3(self):
+        r = mock.Mock(spec=requests.Response)
+        r.status_code = 200
+        r.headers = {"content-type": "application/json"}
+        r.content = b'{"result": ["a", "b", "c"]}'
+
+        self.base_resource._store.update({
+            "session": mock.Mock(spec=requests.Session),
+            "serializer": slumber.serialize.Serializer(),
+        })
+        self.base_resource._store["session"].request.return_value = r
+
+        resp = self.base_resource._request("GET")
+
+        self.assertTrue(resp is r)
+        self.assertEqual(resp.content, r.content)
+
+        self.base_resource._store["session"].request.assert_called_once_with(
+            "GET",
+            "http://example/api/v1/test",
+            data=None,
+            files=None,
+            params=None,
+            headers={"content-type": self.base_resource._store["serializer"].get_content_type(), "accept": self.base_resource._store["serializer"].get_content_type()}
+        )
+
+        resp = self.base_resource.get()
+        self.assertEqual(resp['result'], ['a', 'b', 'c'])
