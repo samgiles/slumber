1337588dafc7a1ee39518198a92e260b0ee83490
Renzo Lucioni <renzo@renzolucioni.com>
2015-05-14 11:01:12 -0400

    Remove string conversion from JsonSerializer, test decoding of Unicode responses

--- tests/resource.py
+++ tests/resource.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 import mock
 import requests
@@ -512,3 +513,33 @@ class ResourceTestCase(unittest.TestCase):
                     },
                 params=getparams,
                 data=None)
+
+    def test_unicode_decodable_response(self):
+        r = mock.Mock(spec=requests.Response)
+        r.status_code = 200
+        r.content = '{"result": "Préparatoire"}'
+        r.headers = {"content-type": "application/json"}
+
+        self.base_resource._store.update({
+            "session": mock.Mock(spec=requests.Session),
+            "serializer": slumber.serialize.Serializer(),
+        })
+        self.base_resource._store["session"].request.return_value = r
+
+        resp = self.base_resource._request("POST")
+
+        self.assertTrue(resp is r)
+        self.assertEqual(resp.content, r.content)
+
+        self.base_resource._store["session"].request.assert_called_once_with(
+            "POST",
+            "http://example/api/v1/test",
+            data=None,
+            files=None,
+            params=None,
+            headers={"content-type": self.base_resource._store["serializer"].get_content_type(), "accept": self.base_resource._store["serializer"].get_content_type()}
+        )
+
+        resp = self.base_resource.post(data={'foo': 'bar'})
+        expected = b'Pr\xc3\xa9paratoire'.decode('utf8')
+        self.assertEqual(resp['result'], expected)
